0ab274e64d689ea09e84a95781b2679cdcfa89cb
coffeerhyder <bkfeedback@pm.me>
2022-08-25 16:35:42 +0200

    Final part of sort fix and bumped version therefore

--- BotNotificator.py
+++ BotNotificator.py
@@ -25,7 +25,7 @@ def notifyUsersAboutNewCoupons(bkbot) -> None:
     logging.info("Checking for pending new coupons notifications")
     timestampStart = datetime.now().timestamp()
     userDB = bkbot.crawler.getUserDB()
-    allNewCoupons = bkbot.crawler.getFilteredCoupons(CouponFilter(activeOnly=True, isNew=True, allowedCouponTypes=BotAllowedCouponTypes, sortCode=CouponSortModes.PRICE))
+    allNewCoupons = bkbot.crawler.getFilteredCoupons(CouponFilter(activeOnly=True, isNew=True, allowedCouponTypes=BotAllowedCouponTypes, sortCode=CouponSortModes.PRICE.getSortCode()))
     if len(allNewCoupons) == 0:
         logging.info("No new coupons available to notify about")
         return
