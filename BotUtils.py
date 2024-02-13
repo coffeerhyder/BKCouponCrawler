6ca4a68b112d0993847854220c12067d1501ea0d
coffeerhyder <bkfeedback@pm.me>
2024-02-13 22:04:37 +0100

    Version 2.1.1 - Changed crawl time to 01:01 to match serverside time - Minor GUI changes - Updated readme

--- BotUtils.py
+++ BotUtils.py
@@ -8,7 +8,7 @@ from telegram import InlineKeyboardMarkup
 
 from Helper import SYMBOLS
 
-VERSION = '2.1.0'
+VERSION = '2.1.1'
 
 """ Place static stuff into this class. """
 
--- QRProcessor.py
+++ QRProcessor.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from pyzbar.pyzbar import decode
 from pyzbar.wrapper import ZBarSymbol
+import cv2
 
 """ 2023-11-25: Playground for automated detection of BK coupons in image / PDF file
  Idea:
@@ -9,14 +10,35 @@ from pyzbar.wrapper import ZBarSymbol
  3. OCR each image to find the remaining data.
  4. Put the data into our database so the bot can make use of it.
  """
-img = Image.open('bk.png')
 
+imagepath = 'bk.png'
+
+# pyzbar
+img = Image.open(imagepath)
 decoded_list = decode(img, symbols=[ZBarSymbol.QRCODE])
-
-print(type(decoded_list))
+print(f'result 1: {decoded_list}')
 # <class 'list'>
 
-print(f'Number of detected QR codes: {len(decoded_list)}')
-print(f'{decoded_list}')
+# img = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
+# clean_im = cv2.medianBlur(img, 25)  # Apply median blur for reducing noise
+# Show image for testing
+# cv2.imshow('small_clean_im', clean_im)
+
+image = cv2.imread(imagepath)
+grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+height, width = image.shape[:2]
+decoded_list = decode((grey.tobytes(), width, height))
+print(f'result grey: {decoded_list}')
+
+
+
+decoded_list = decode(img, symbols=[ZBarSymbol.QRCODE])
+print(f'result 2: {decoded_list}')
+
+
+qcd = cv2.QRCodeDetector()
+
+retval, decoded_info, points, straight_qrcode = qcd.detectAndDecodeMulti(img)
+print(f'result 3: {decoded_info=} | {points=}')
 
 print("End")
